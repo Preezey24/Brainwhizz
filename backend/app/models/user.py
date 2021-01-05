eda4c5db4af2209e06b7950bd87142808da36547
Rhys Previte <previterp88@gmail.com>
2021-01-05 16:09:16 -0500

    Completed models folder with User model

---
+++ backend/app/models/user.py
@@ -0,0 +1,24 @@
+from .db import db 
+from werkzeug.security import generate_password_hash, check_password_hash 
+from flask-login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin): 
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50) nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password): 
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password) 
+        return check_password_hash(self.password, password)
