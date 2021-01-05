9911ceec16fa54ab87c5f2a088ae411f2cacaf5c
Rhys Previte <previterp88@gmail.com>
2021-01-05 16:31:22 -0500

    Completing migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
 
 from .config import Config   
-from .models import db 
+from app.models import db 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+Migrate(app, db)
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -22,3 +22,10 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password) 
         return check_password_hash(self.password, password)
+    
+    def to_dict(self): 
+        return {
+            "id": self.id, 
+            "username": self.username, 
+            "email": self.email, 
+        }
