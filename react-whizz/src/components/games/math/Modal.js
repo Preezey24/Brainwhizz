98ae18e28463a36ddec0eac4551dc87493e8d0d8
Rhys Previte <previterp88@gmail.com>
2021-01-18 19:59:10 -0500

    Added modal styling for math game

--- react-whizz/src/components/games/math/Modal.js
+++ react-whizz/src/components/games/math/Modal.js
@@ -1,36 +1,17 @@
 import React from 'react';
-
-const MODAL_STYLES = {
-    position: 'fixed', 
-    top: '50%', 
-    left: '50%', 
-    transform: 'translate(-50%, -50%)', 
-    backgroundColor: 'red', 
-    padding: '50px', 
-    zIndex: 1000
-}
-
-const OVERLAY_STYLE = {
-    position: 'fixed', 
-    top: 0, 
-    left: 0, 
-    right: 0, 
-    bottom: 0, 
-    backgroundColor: 'rgba(0, 0, 0, 0.7)', 
-    zIndex: 1000, 
-}
+import './Modal.css';
 
 const Modal = ({open, score, playAgain, gameScore, exitGame}) => {
     if (!open) return null;    
     
     return (
         <>
-            <div style={OVERLAY_STYLE} />
-            <div style={MODAL_STYLES}>
-                <button onClick={playAgain}>Play Again</button>
-                <button onClick={exitGame}>Home Page</button>
-                <p>{gameScore.current}</p>
-                <p>{score.current}</p>
+            <div className={"overlay"} />
+            <div className={"modal"}>
+                <button onClick={playAgain} className={"button__again"}>Play Again</button>
+                <button onClick={exitGame} className={"button__home"}>Home</button>
+                <span className={"text__game"}>Score: {gameScore.current}</span>
+                <span className={"text__session"}>Session Score: {score.current}</span>
             </div>
         </>
     )
