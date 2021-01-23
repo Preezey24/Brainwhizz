54c7b3bf7cab62d1ceaa2b710acfb4049102fb0b
Rhys Previte <previterp88@gmail.com>
2021-01-23 11:41:05 -0500

    Completed nav bar

--- react-whizz/src/components/footer/Footer.js
+++ react-whizz/src/components/footer/Footer.js
@@ -11,9 +11,11 @@ const Footer = () => {
                     <IconContext.Provider value={{className: "footer_icon_git"}}>
                         <GrGithub/>
                     </IconContext.Provider>
-                    <a className={"footer_link"} href= 'https://github.com/Preezey24/Brainwhizz'>
-                        Brainwhizz
-                    </a>
+                    <span style={{height: "50%", position: "absolute", top: "35%"}}>
+                        <a className={"footer_link"} href= 'https://github.com/Preezey24/Brainwhizz'>
+                            Brainwhizz
+                        </a>
+                    </span>
                 </span>
             </div>
             <div className={"footer_me"}>
@@ -23,7 +25,7 @@ const Footer = () => {
                             <GrLinkedin/>
                         </IconContext.Provider>
                     </span>
-                    <span style={{height: "50%", position: "absolute", top: "20%"}}>
+                    <span style={{height: "50%", position: "absolute", top: "15%"}}>
                         <a className={"footer_link"} href= 'https://www.linkedin.com/in/rhysprevite/'>
                             Rhys Previte
                         </a>
